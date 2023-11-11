03798ea63ef345f2c67ab076ffb905d085572f0a
Tidic <tidic84@gmail.com>
2023-11-11 01:24:54 +0100

    background + sections

--- js/cursor.js
+++ js/cursor.js
@@ -1,23 +1,45 @@
 const cursorDot = document.querySelector('.cursor-dot');
 const cursorOutline = document.querySelector('.cursor-outline');
 const links = document.querySelectorAll('a');
+function load() {
+    var ev = window.event;
+    cursorDot.style.left = `${ev.clientX}px`;
+    cursorDot.style.top = `${ev.clientY}px`;
+    console.log(ev.clientX);
+}
 
-window.addEventListener('mousemove', (e) => {
-    const posX = e.clientX;
-    const posY = e.clientY;
 
+window.addEventListener('mousemove', trackMouse, false);
+window.addEventListener('mouseenter', trackMouse, false);
+document.addEventListener('mouseleave', () => {
+    cursorDot.style.opacity = 0;
+    cursorOutline.style.opacity = 0;
+});
+
+function trackMouse(e) {
+    console.log(e.clientX);
+    var posX = e.clientX;
+    var posY = e.clientY;
+    if(posX != undefined && posY != undefined) {
+        cursorDot.style.left = `${posX}px`;
+        cursorDot.style.top = `${posY}px`;
+        cursorDot.style.opacity = 1;
+        cursorOutline.style.opacity = 1;
+    }
     cursorDot.style.left = `${posX}px`;
     cursorDot.style.top = `${posY}px`;
 
-    cursorOutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    }, {
-        duration: 400,
-        fill: 'forwards',
-        easing: 'ease-in'
-    });
-});
+    if(cursorDot.style.opacity == 1) {
+        cursorOutline.animate({
+            left: `${posX}px`,
+            top: `${posY}px`
+        }, {
+            duration: 400,
+            fill: 'forwards',
+            easing: 'ease-in'
+        });
+    }
+}
 
 links.forEach(link => {
     link.addEventListener('mouseenter', () => {
